0a200ad4476d59303ff81cc84b740fb6a2d2aca8
md-forhad <forhadkhan20330@gmail.com>
2022-12-27 08:29:39 +0600

    add string in python function

--- addition of two integer number.py
+++ addition of two integer number.py
@@ -1,4 +1,4 @@
 number1 =int(input('Enter the value of number1: '))
 number2 =int(input('Enter the value of number2: '))
 sum = number1 + number2
-print('Addition of two number: ', sum)
+print('Addition of two number : ', sum)
